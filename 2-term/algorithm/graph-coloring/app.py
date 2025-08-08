13a40447b262adc6b448a199506077ad534d9d7a
Quang Hung Pham <69830292+hungpq7@users.noreply.github.com>
2025-08-08 22:51:17 +0700

    -

--- 2-term/algorithm/graph-coloring/app.py
+++ 2-term/algorithm/graph-coloring/app.py
@@ -143,7 +143,7 @@ current_colors, current_explanation = steps[st.session_state.step_idx]
 with col12:
     # set_colors = set([c for c in current_colors.values() if c > 0])
     # st.metric("Colors used", len(set_colors))
-    st.write(type(current_colors))
+    st.write(str(current_colors))
 
 
 def draw_graph(colors):
