65521557996d4ef3dbc824e24530eb262591204e
Quang Hung Pham <69830292+hungpq7@users.noreply.github.com>
2025-10-03 21:56:26 +0700

    -

--- 2-term/algorithm/pagerank/pages/1_Exploring_Graph.py
+++ 2-term/algorithm/pagerank/pages/1_Exploring_Graph.py
@@ -100,7 +100,7 @@ for nid, attrs in NODES.items():
         "SeedPage": nid in TRUSTRANK_GOOD_SEEDS
     })
 df_nodes = pd.DataFrame(rows).sort_values("PageID").reset_index(drop=True)
-st.dataframe(df_nodes, use_container_width=False)
+st.dataframe(df_nodes, hide_index=True, width='content')
 
 # ----------------------
 # 2) Graph (Graphviz)
